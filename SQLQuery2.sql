1e4979c76ffae9a06ef3e6581066258a48eb1805
Ömercan Göktaş <77095758+omercngoktas@users.noreply.github.com>
2022-02-22 21:08:53 +0300

    u

---
+++ SQLQuery2.sql
@@ -0,0 +1,40 @@
+declare @noOfEmp int
+set @noOfEmp = (select count(*) from employee)
+declare @noOfOrders int
+set @noOfOrders = (select count(*) from cust_order)
+declare @counter int
+set @counter = 0
+
+WHILE(@counter < (@noOfOrders / @noOfEmp))
+BEGIN
+	WITH employee_rows AS(
+		SELECT e.employee_id, ROW_NUMBER() OVER (ORDER BY NEWID()) AS rn
+		FROM employee e),
+	custorder_rows AS(
+		SELECT co.order_id, ROW_NUMBER() OVER (ORDER BY NEWID()) AS rn
+		FROM cust_order co
+		WHERE co.employee_id is NULL
+		)
+	UPDATE co SET co.employee_id = er.employee_id
+	FROM employee_rows er, custorder_rows cor, cust_order co
+	WHERE er.rn = cor.rn AND co.order_id = cor.order_id
+
+	set @counter = @counter + 1
+END
+
+WITH employee_rows AS(
+	SELECT e.employee_id, ROW_NUMBER() OVER (ORDER BY NEWID()) AS rn
+	FROM employee e),
+custorder_rows AS(
+	SELECT co.order_id, ROW_NUMBER() OVER (ORDER BY NEWID()) AS rn
+	FROM cust_order co
+	WHERE co.employee_id is NULL
+	)
+UPDATE co SET co.employee_id = er.employee_id
+FROM employee_rows er, custorder_rows cor, cust_order co
+WHERE er.rn = cor.rn AND co.order_id = cor.order_id
+
+
+select * from cust_order co where co.employee_id is not NULL
+update custome set custome.employee_id = NULL from cust_order custome
+update custome set custome.employee_id = 1 from cust_order custome where custome.order_id > 15
